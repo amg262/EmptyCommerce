c445fb7841fac902c8384661b1b3102192320a57
Andrew Gunn <andrewgunn31@gmail.com>
2023-03-23 11:17:58 -0500

    Empty page controller and related files for another page type

---
+++ EmptyPageController.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmptyCommerce
+{
+    public class EmptyPageController : PageController<EmptyPage>
+    {
+        [HttpGet]
+        public IActionResult Index(EmptyPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
